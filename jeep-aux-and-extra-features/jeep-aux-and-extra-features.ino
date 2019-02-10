f9a15ee6cae873ae95fefa1be1983a551f166e1c
Anton Viktorov <latonita@yandex.ru>
2019-02-11 00:31:56 +0300

    pcb fix, sw pins update

--- jeep-aux-and-extra-features/jeep-aux-and-extra-features.ino
+++ jeep-aux-and-extra-features/jeep-aux-and-extra-features.ino
@@ -1,45 +1,44 @@
 /*****************************************************************************************
-*
-* Hack for my Jeep Grand Cherokee WH 2006 (european) + RAR radio head unit
-*
-* My hardware:
-*  1. Arduino Pro Mini 5v/16Mhz
-*  2. Mcp2515_can SPI module (8Mhz)
-*  3. Generic (H166) bluetooth A2DP module based on OVC3860, UART connected to Arduino
-*
-* Features:
-*  1. Bench mode to enable radio functioning while removed from the car
-*  2. Emulating VES presense to enable AUX IN in head unit
-*  2.1. Turn on/off BT A2DP module only when AUX IN is selected
-*  3. Control BT audio source with seek buttons
-*  4. [todo] Signal comfort module - blinkers blink three times instead of one.
-*  5. [todo] Headlights - always on
-*  6. [todo] Auto wipers - require installation of rain sensor (? maybe it will work alone, w/o my software :)
-*  7. [todo] Auto parameters display
-*  7.1. gasoline cost for a trip
-*
-* Copyright (C) 2015-2017 Anton Viktorov <latonita@yandex.ru>
-*                                    https://github.com/latonita/jeep-canbus
-*
-* This is free software. You may use/redistribute it under The MIT License terms.
-*
-*****************************************************************************************/
+ *
+ * Hack for my Jeep Grand Cherokee WH 2006 (european) + RAR radio head unit
+ *
+ * My hardware:
+ *  1. Arduino Pro Mini 5v/16Mhz
+ *  2. Mcp2515_can SPI module (8Mhz)
+ *  3. Generic (H166) bluetooth A2DP module based on OVC3860, UART connected to Arduino
+ *
+ * Features:
+ *  1. Bench mode to enable radio functioning while removed from the car
+ *  2. Emulating VES presense to enable AUX IN in head unit
+ *  2.1. Turn on/off BT A2DP module only when AUX IN is selected
+ *  3. Control BT audio source with seek buttons
+ *  4. [todo] Signal comfort module - blinkers blink three times instead of one.
+ *  5. [todo] Headlights - always on
+ *  6. [todo] Auto wipers - require installation of rain sensor (? maybe it will work alone, w/o my software :)
+ *  7. [todo] Auto parameters display
+ *  7.1. gasoline cost for a trip
+ *
+ * Copyright (C) 2015-2017 Anton Viktorov <latonita@yandex.ru>
+ *                                    https://github.com/latonita/jeep-canbus
+ *
+ * This is free software. You may use/redistribute it under The MIT License terms.
+ *
+ *****************************************************************************************/
 #include <SoftwareSerial.h>
 #include <SPI.h>
 #include "mcp_can.h"
 
-#define CAN_MODULE_CS_PIN 10
+#define CAN_MODULE_CS_PIN 10 // PB2
 
-//A1
-#define BLUETOOTH_SWITCH 15 
+#define BLUETOOTH_SWITCH A1 // 15 //PC1
 
 // BT TX -------- RX A4(18)
-// BT RX -- >|--- TX A5(19)
-// My BT module is 3.3v, but arduino is 5v. 
+// BT RX -- >|--- TX A2(19)
+// My BT module is 3.3v, but arduino is 5v.
 // Need to put blocking diode: do not let Arduinos 5v get to BT, but let it easily pulldown a line
 // Any diode with low Vf will do. In my case I put Schottky IN5819 desoldered from some dead device
-#define BLUETOOTH_TX 18
-#define BLUETOOTH_RX 19
+#define BLUETOOTH_TX A4
+#define BLUETOOTH_RX A2
 SoftwareSerial btSerial(BLUETOOTH_TX, BLUETOOTH_RX);
 
 #define CHECK_PERIOD_MS 200
@@ -57,11 +56,11 @@ unsigned long lastAnnounce = 0;
 unsigned long lastButtonPress = 0;
 
 #define msgVesAuxModeLen 8
-unsigned char msgVesAuxMode[8] = {3,0,0,0,0,0,0,0};
+unsigned char msgVesAuxMode[8] = {3, 0, 0, 0, 0, 0, 0, 0};
 
 #ifdef BENCH_MODE_ON
 #define msgPowerOnLen 6
-unsigned char msgPowerOn[6]= {0x63,0,0,0,0,0};
+unsigned char msgPowerOn[6] = {0x63, 0, 0, 0, 0, 0};
 #endif
 
 #define RADIOMODE_OTHER 0
@@ -70,36 +69,34 @@ unsigned char radioMode = RADIOMODE_OTHER;
 
 const char compileDate[] = __DATE__ " " __TIME__;
 
-
-#define PIN_DOWN(x) pinMode(x,OUTPUT); \
-                    digitalWrite(x,LOW);
+#define PIN_DOWN(x)                                                                                                                                  \
+  pinMode(x, OUTPUT);                                                                                                                                \
+  digitalWrite(x, LOW);
 
 void pinsSetup() {
-/*  PIN_DOWN(2);
-  PIN_DOWN(3);
-  PIN_DOWN(4);
-  PIN_DOWN(5);
-  PIN_DOWN(6);
-  PIN_DOWN(7);
-  PIN_DOWN(8);
-  PIN_DOWN(9);
-*/
+  /*  PIN_DOWN(2);
+    PIN_DOWN(3);
+    PIN_DOWN(4);
+    PIN_DOWN(5);
+    PIN_DOWN(6);
+    PIN_DOWN(7);
+    PIN_DOWN(8);
+    PIN_DOWN(9);
+  */
   pinMode(BLUETOOTH_SWITCH, OUTPUT);
   digitalWrite(BLUETOOTH_SWITCH, LOW);
 }
 
-
-#define CAN_POWER          0x000
-#define CAN_BLINKERS       0x006
-#define CAN_RADIO_MODE     0x09f
+#define CAN_POWER 0x000
+#define CAN_BLINKERS 0x006
+#define CAN_RADIO_MODE 0x09f
 #define CAN_RADIO_CONTROLS 0x394
-#define CAN_VES_UNIT       0x3dd
+#define CAN_VES_UNIT 0x3dd
 
 //#define CAN_MULTI_SWITCH   0x11d
 //#define CAN_HEADLIGHTS 0x1c8
 
-
-//void setupFilters() {
+// void setupFilters() {
 //  CAN.init_Mask(0, 0, 0x3ff);
 //  CAN.init_Mask(1, 0, 0x3ff);
 //
@@ -112,15 +109,15 @@ void pinsSetup() {
 //}
 
 void setup() {
-//  pinsSetup();
+  //  pinsSetup();
 
   Serial.begin(115200);
   Serial.print("Jeep VES Enabler + Extras by latonita v.1.1, ");
   Serial.println(compileDate);
-  
+
   btSerial.begin(115200);
 
-  while(CAN_OK != CAN.begin(CAN_83K3BPS, MCP_8MHz)) {
+  while (CAN_OK != CAN.begin(CAN_83K3BPS, MCP_8MHz)) {
     Serial.println("CAN init fail");
     delay(250);
   }
@@ -132,13 +129,13 @@ void setup() {
 
 void sendAnnouncements() {
 #ifdef BENCH_MODE_ON
-    // when on bench - send power on command to radio to enable it
-    CAN.sendMsgBuf(CAN_POWER, 0, msgPowerOnLen, msgPowerOn);
-    delay(CAN_DELAY_AFTER_SEND);
+  // when on bench - send power on command to radio to enable it
+  CAN.sendMsgBuf(CAN_POWER, 0, msgPowerOnLen, msgPowerOn);
+  delay(CAN_DELAY_AFTER_SEND);
 #endif
-    //tell them VES AUX is here
-    CAN.sendMsgBuf(CAN_VES_UNIT, 0, msgVesAuxModeLen, msgVesAuxMode);
-    delay(CAN_DELAY_AFTER_SEND);
+  // tell them VES AUX is here
+  CAN.sendMsgBuf(CAN_VES_UNIT, 0, msgVesAuxModeLen, msgVesAuxMode);
+  delay(CAN_DELAY_AFTER_SEND);
 }
 
 unsigned int canId = 0;
@@ -148,68 +145,68 @@ unsigned char newMode = 0;
 
 void checkIncomingMessages() {
 
-  if(CAN_MSGAVAIL != CAN.checkReceive())
+  if (CAN_MSGAVAIL != CAN.checkReceive())
     return;
-    
+
   CAN.readMsgBuf(&len, buf);
   canId = CAN.getCanId();
 
   switch (canId) {
-    case CAN_RADIO_MODE:
+  case CAN_RADIO_MODE:
 
-        // some debug output
-          Serial.print("CAN ID: ");
-          Serial.print(canId, HEX);
+    // some debug output
+    Serial.print("CAN ID: ");
+    Serial.print(canId, HEX);
 
-          for (int i = 0; i< len; i++) {
-            Serial.print(",");
-            Serial.print(buf[i],HEX);
-          }
-          Serial.println();
-          Serial.print("Radio mode: ");
-          Serial.print(buf[0] & 0xF, HEX);
-          Serial.print(":");
-          Serial.print(radioMode);
-          Serial.print(">");
-          Serial.println(newMode);
+    for (int i = 0; i < len; i++) {
+      Serial.print(",");
+      Serial.print(buf[i], HEX);
+    }
+    Serial.println();
+    Serial.print("Radio mode: ");
+    Serial.print(buf[0] & 0xF, HEX);
+    Serial.print(":");
+    Serial.print(radioMode);
+    Serial.print(">");
+    Serial.println(newMode);
 
-      newMode = ((buf[0] & 0xF) == 6) ? RADIOMODE_AUX : RADIOMODE_OTHER;
+    newMode = ((buf[0] & 0xF) == 6) ? RADIOMODE_AUX : RADIOMODE_OTHER;
 
-      if (radioMode != newMode) {
-        radioMode = newMode;
-        digitalWrite(BLUETOOTH_SWITCH, radioMode == RADIOMODE_AUX ? HIGH : LOW);
-        if (radioMode == RADIOMODE_AUX) {
-          Serial.print("Radio Mode changed to AUX");
-        } else {
-          Serial.print("Radio Mode changed to something else");
-        }
+    if (radioMode != newMode) {
+      radioMode = newMode;
+      digitalWrite(BLUETOOTH_SWITCH, radioMode == RADIOMODE_AUX ? HIGH : LOW);
+      if (radioMode == RADIOMODE_AUX) {
+        Serial.print("Radio Mode changed to AUX");
+      } else {
+        Serial.print("Radio Mode changed to something else");
       }
-      break;
-    case CAN_RADIO_CONTROLS:
-      // radio mode + buttons
-      if (buf[3] > 0 && millis() > lastButtonPress + BUTTON_PRESS_DEBOUNCE_MS) { // something pressed
-        lastButtonPress = millis();
-        if (buf[3] & 1) { // seek right
-          Serial.println ("-- Seek >>");
-          Serial.write("AT#MD\n\r");
-          btSerial.write("AT#MD\n\r");
-        } else if (buf[3] & 2) { // seek left
-          Serial.println ("<< Seek --");
-          Serial.write("AT#ME\n\r");
-          btSerial.write("AT#ME\n\r");
-        } else if (buf[3] & 4) { // rw/ff right
-          Serial.println ("-- RW FF >>");
-        } else if (buf[3] & 8) { // rw/ff left
-          Serial.println ("<< RW FF --");
-        } else if (buf[3] & 0x20) { // RND/PTY
-          Serial.println ("[RND/PTY] - use as play/pause");
-          Serial.write("AT#MA\n\r");
-          btSerial.write("AT#MA\n\r");
-        }
+    }
+    break;
+  case CAN_RADIO_CONTROLS:
+    // radio mode + buttons
+    if (buf[3] > 0 && millis() > lastButtonPress + BUTTON_PRESS_DEBOUNCE_MS) { // something pressed
+      lastButtonPress = millis();
+      if (buf[3] & 1) { // seek right
+        Serial.println("-- Seek >>");
+        Serial.write("AT#MD\n\r");
+        btSerial.write("AT#MD\n\r");
+      } else if (buf[3] & 2) { // seek left
+        Serial.println("<< Seek --");
+        Serial.write("AT#ME\n\r");
+        btSerial.write("AT#ME\n\r");
+      } else if (buf[3] & 4) { // rw/ff right
+        Serial.println("-- RW FF >>");
+      } else if (buf[3] & 8) { // rw/ff left
+        Serial.println("<< RW FF --");
+      } else if (buf[3] & 0x20) { // RND/PTY
+        Serial.println("[RND/PTY] - use as play/pause");
+        Serial.write("AT#MA\n\r");
+        btSerial.write("AT#MA\n\r");
       }
+    }
 
-    default:
-      break;
+  default:
+    break;
   }
 }
 void loop() {
